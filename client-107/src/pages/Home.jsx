579e8251afc09ddbe39582e076d5441124e89b38
Siwanart <xdalone007@gmail.com>
2024-11-04 16:49:35 +0700

    Add files via upload

---
+++ client-107/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+// rafce
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+        Home page
+      </h1>
+    </div>
+  )
+}
+
+export default Home
